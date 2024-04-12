3221dc9a3e7010f2d20ba94d148609f188c05770
bauthyba <barberogaticabautista@gmail.com>
2024-04-12 11:27:42 -0300

    Renombrar carpetas y primer ejercicio de la guia 3

---
+++ 03-Ejercicios_Excepciones/Ejercicio-1.py
@@ -0,0 +1,18 @@
+while True:
+    try:
+        num1 = int(input("Ingrese el primer numero entero: "))
+        num2 = int(input("Ingrese el segundo numero entero: "))
+        suma = num1 + num2
+        print("La suma de", num1, "y", num2, "es:", suma)
+
+        while True:
+            opcion = input("¿Desea seguir sumando valores? (1: si - 0: No): ")
+            if opcion == "1":
+                break
+            elif opcion == "0":
+                exit()
+            else:
+                print("Opcion invalida. Por favor, ingrese 1 para continuar o 0 para salir.")
+
+    except ValueError:
+        print("Por favor, ingrese solo numeros enteros.")
